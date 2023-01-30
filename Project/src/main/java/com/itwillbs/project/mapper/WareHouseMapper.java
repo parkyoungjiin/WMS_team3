ab4358d60c98d8cf0309c0bc4f98401668a554a3
taehun6884 <112138204+taehun6884@users.noreply.github.com>
2023-01-30 18:14:57 +0900

    01-30 작업
    코드 중복 확인/ 수정

--- Project/src/main/java/com/itwillbs/project/mapper/WareHouseMapper.java
+++ Project/src/main/java/com/itwillbs/project/mapper/WareHouseMapper.java
@@ -13,11 +13,15 @@ public interface WareHouseMapper {
 	//------------창고 목록 조회 작업-----------------
 	List<WareHouseVO> getwhList();
 	//------------창고 상세 내용 작업-----------------
-	WareHouseVO getWarehouse(String wh_ch);
+	WareHouseVO getWarehouse(String wh_cd);
 	//------------창고 상세 내용(이름) 작업-----------------
 	WareHouseVO getWarehouseName(@Param("wh_cd") String wh_cd,
 								@Param("wh_name")String wh_name
 								);
-	void whModify(String wh_cd);
-
+	//------------창고 수정 작업-----------------
+	void whModify(
+			@Param("wh") WareHouseVO vo,
+			@Param("wh_cd") String wh_cd);
+	//------------창고 코드 중복 확인 작업-----------------
+	int getDB_wh_cd(String wh_cd);
 }
